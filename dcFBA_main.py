9d41f0b83767c30e9c7fcf27a140ef395d2846e6
morof17 <moro.francesco17@gmail.com>
2022-01-25 17:49:20 +0100

    Changed named Add description of the purpose of the file

---
+++ dcFBA_main.py
@@ -0,0 +1,4 @@
+# DynamicCommunityFBA_main
+# run the complete pipeline (all methods)
+# single methods can be skipped changing the correspondent flag in the parameter file
+# TODO: create a parameter file "dcFBA_main_simulationParameters"
