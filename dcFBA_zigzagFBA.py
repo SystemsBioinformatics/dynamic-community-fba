a87ea119bd986151839e4ae3943b4d2ee104ff29
morof17 <moro.francesco17@gmail.com>
2022-01-26 10:10:38 +0100

    Aggiunte altre idee a caso

--- dcFBA_zigzagFBA.py
+++ dcFBA_zigzagFBA.py
@@ -1,1 +1,11 @@
 # promemoria per idea a caso :/:\:
+
+# towFBA
+# tiro alla fine
+# chi vince? Tra chi?
+# Tra organismi
+# biforcazione -<=>- (Frank + classic)
+# sovrapposizione risultati: f onda mq?
+
+# triangular: same step 2a+1b->a poi viceversa
+# parallel: FRANK(a+b) + classic a + classic b tutto assieme, ogni t si mischia
