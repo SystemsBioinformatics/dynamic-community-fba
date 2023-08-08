3d5a70cae4fe6e2a0a4530d93d08b9b911563c0f
Steven Wijnen <steven.wijnen@icloud.com>
2023-08-08 10:35:37 +0200

    scriptje

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import cbmpy
 from cbmpy.CBModel import Model
 from DCFBA.Models import CommunityModel
-from DCFBA.DynamicModels import DynamicParallelFBA
+from DCFBA.DynamicModels import DynamicSingleFBA
 
 
 model1: Model = cbmpy.loadModel("models/bigg_models/e_coli_core.xml")
@@ -18,19 +18,24 @@ for rid in model2.getReactionIds():
         reaction = model2.getReaction(rid)
         reaction.is_exchange = True
 
-model1.getReaction("R_GLCpts").setUpperBound(10)
-model2.getReaction("R_GLCpts").setUpperBound(6)
+model1.getReaction("R_GLCpts").setUpperBound(3)
+model2.getReaction("R_GLCpts").setUpperBound(10)
+model2.getReaction("R_LCTSGALex").setUpperBound(8)
+model2.getReaction("R_LCTSt6").setUpperBound(3)
 
-parallel_fba = DynamicParallelFBA(
-    [model2, model1],
-    [1.0, 1.0],
-    {"M_glc__D_e": 100, "M_gal_e": 0, "M_lcts_e": 100},
+
+parallel_fba = DynamicSingleFBA(
+    model2,
+    "R_biomass_STR",
+    1.0,
+    {"M_glc__D_e": 0, "M_gal_e": 0, "M_lcts_e": 100},
 )
 
-T, metabolites, biomasses = parallel_fba.simulate(0.1)
 
-plt.plot(T, metabolites["M_glc__D_e"][:-1], color="blue", label="[Glucose]")
-plt.plot(T, metabolites["M_lcts_e"][:-1], color="orange", label="[Lactose]")
+T, metabolites, biomasses, _ = parallel_fba.simulate(0.1)
+print(model2.getReaction("R_EX_gal_e").getLowerBound())
+plt.plot(T, metabolites["M_gal_e"], color="blue", label="[Glucose]")
+plt.plot(T, metabolites["M_lcts_e"], color="orange", label="[Lactose]")
 
 plt.xlabel("Time")
 plt.ylabel("Concentration")
@@ -38,8 +43,8 @@ plt.legend()
 plt.show()
 
 # plt.plot(T, biomasses["ecoli"], color="blue", label="modelA")
-plt.plot(T, biomasses[model1.getId()], color="orange", label="ecoli")
-plt.plot(T, biomasses[model2.getId()][:-1], color="blue", label="strep")
+plt.plot(T, biomasses[""], color="orange", label="ecoli")
+# plt.plot(T, biomasses[model2.getId()], color="blue", label="strep")
 
 plt.xlabel("Time")
 plt.ylabel("Concentration")
