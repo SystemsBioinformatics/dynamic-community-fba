6a18c654ee3e6bbf09815c79c3575bb880fdcbdd
Steven Wijnen <steven.wijnen@icloud.com>
2023-09-08 15:13:24 +0200

    testing

--- script.py
+++ script.py
@@ -1,24 +1,15 @@
-import cbmpy
 import matplotlib.pyplot as plt
 from cbmpy.CBModel import Model, Reaction
 from DCFBA.ToyModels import model_a, model_b
 from DCFBA.Models.CommunityModel import CommunityModel
+from DCFBA.DynamicModels.DynamicJointFBA import DynamicJointFBA
 from DCFBA.Models.Kinetics import KineticsStruct
-from DCFBA.DynamicModels import EndPointFBA
-from DCFBA.Helpers.PlotsEndPointFBA import plot_biomasses, plot_metabolites
 
 m_a: Model = model_a.build_toy_model_fba_A()
-
 m_a.getReaction("R_1").setUpperBound(10)
 m_a.getReaction("R_4").setUpperBound(3)
 m_a.getReaction("R_6").setUpperBound(1)
 
-# Need to delete biomass model, since it is created in the endPoint model
-reaction: Reaction = m_a.getReaction("R_BM_A").deleteReagentWithSpeciesRef(
-    "BM_e_A"
-)
-
-# m_a.getReaction("R_import_B").setUpperBound(1)
 
 m_a.getReaction("R_1").setLowerBound(0)
 m_a.getReaction("R_4").setLowerBound(0)
@@ -28,10 +19,6 @@ m_b: Model = model_b.build_toy_model_fba_B()
 m_b.getReaction("R_1").setUpperBound(10)
 m_b.getReaction("R_3").setUpperBound(1)
 m_b.getReaction("R_5").setUpperBound(1)
-# Need to delete biomass model, since it is created in the endPoint model
-reaction: Reaction = m_b.getReaction("R_BM_B").deleteReagentWithSpeciesRef(
-    "BM_e_B"
-)
 
 
 m_b.getReaction("R_1").setLowerBound(0)
@@ -39,43 +26,56 @@ m_b.getReaction("R_3").setLowerBound(0)
 m_b.getReaction("R_5").setLowerBound(0)
 
 
-kin = KineticsStruct(
-    {
-        "R_1_modelA": ["", 10, 10],
-        "R_4_modelA": ["B_e", 5, 3],
-        "R_6_modelA": ["B_e", 3, 1],
-        # B
-        "R_1_modelB": ["", 10, 10],
-        "R_3_modelB": ["A_e", 2, 1],
-    }
-)
-
-
 community_model = CommunityModel(
     [m_a, m_b], ["R_BM_A", "R_BM_B"], ["modelA", "modelB"]
 )
 
-# Are set by the model as often required by other GSMM
-community_model.deleteReactionAndBounds("BM_e_A_exchange")
-community_model.deleteReactionAndBounds("BM_e_B_exchange")
 
-# community_model.getReaction("B_exchange").setLowerBound(-100)
-# community_model.getReaction("A_exchange").setLowerBound(-100)
+community_model.getReaction("S_e_exchange").setLowerBound(-100000)
+community_model.getReaction("A_e_exchange").setLowerBound(-100000)
+community_model.getReaction("B_e_exchange").setLowerBound(-100000)
 
-
-# community_model.getReaction("S_exchange").setLowerBound(-100)
-
-n = 21
-dj = EndPointFBA(
-    community_model,
-    n,
-    {"modelA": 1.0, "modelB": 2.0},
-    {"S_e": 100, "A_e": 0.0, "B_e": 0.0},
-    0.1,
+dj = DynamicJointFBA(
+    community_model, [1, 1], {"S_e": 100, "A_e": 0.001, "B_e": 0.001}
 )
+T, metabolites, biomasses, fluxes = dj.simulate(0.1)
+index = 0
 
 
-solution = dj.simulate()
+rv1 = list(map(lambda d: d["R_1_modelA"], fluxes))
+rv2 = list(map(lambda d: d["R_2_modelA"], fluxes))
+rv3 = list(map(lambda d: d["R_3_modelA"], fluxes))
+rv4 = list(map(lambda d: d["R_4_modelA"], fluxes))
+rv5 = list(map(lambda d: d["R_5_modelA"], fluxes))
+rv6 = list(map(lambda d: d["R_6_modelA"], fluxes))
 
-plot_biomasses(dj)
-plot_metabolites(dj, {"S_e": 100, "A_e": 0.0, "B_e": 0.0})
+
+plt.plot(T, metabolites["S_e"], color="blue", label="Metabolite s")
+plt.xlabel("Time")
+plt.ylabel("Concentration")
+plt.legend()
+plt.show()
+
+
+plt.plot(T, metabolites["A_e"], color="blue", label="Metabolite a")
+plt.xlabel("Time")
+plt.ylabel("Concentration")
+plt.legend()
+plt.show()
+
+
+# Adding labels and title
+
+plt.plot(T, metabolites["B_e"], color="blue", label="Metabolite B")
+plt.xlabel("Time")
+plt.ylabel("Concentration")
+# Adding legend
+plt.legend()
+plt.show()
+
+plt.plot(T, biomasses["modelA"], color="blue", label="modelA")
+plt.plot(T, biomasses["modelB"], color="orange", label="modelB")
+plt.xlabel("Time")
+plt.ylabel("Concentration")
+plt.legend()
+plt.show()
