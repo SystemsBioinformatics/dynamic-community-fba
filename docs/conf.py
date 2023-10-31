c8ad63256089fe920a88a93e0971c0986f3ffa70
Steven Wijnen <steven.wijnen@icloud.com>
2023-10-31 10:05:48 +0100

    working on docs

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@ import sys
 import os
 
 
-# sys.path.insert(0, os.path.abspath("./"))
+sys.path.insert(0, os.path.abspath("./"))
 project = "dynamic-community-fba"
 copyright = "2023, S.C.M.A. Wijnen, F. Moro"
 authors = ["S.C.M.A. Wijnen", "F. Moro"]
